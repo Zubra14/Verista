09781fb30727812b62d04e6453c0ad1b446d87c3
Zubra14 <znkonjane@gmail.com>
2025-03-21 11:21:55 +0200

    Fix .gitignore to include frontend files

--- routes/protectedRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { authenticateUser, isAdmin } = require("../middleware/authMiddleware");
-const router = express.Router();
-
-router.get("/admin-only", authenticateUser, isAdmin, (req, res) => {
-    res.json({ message: "Welcome Admin! You have access to this route." });
-});
-
-module.exports = router;
